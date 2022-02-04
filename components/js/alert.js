546334249ec149e0f5b8fbdb8646037a1053f17c
Hinanshi Suthar <hinanshis4@gmail.com>
2022-02-04 18:09:27 +0530

    feat: close alert functionality

---
+++ components/js/alert.js
@@ -0,0 +1,6 @@
+const alertCloseBox = document.querySelector("#alert-close");
+const alertCloseBtn = document.querySelector("#alert-close-btn");
+
+alertCloseBtn.addEventListener("click",()=>{
+    alertCloseBox.style.display = "none";
+})
