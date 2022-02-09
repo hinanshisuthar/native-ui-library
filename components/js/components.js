e290fd5608d3536124aa101569e3dd661bda6adb
Hinanshi Suthar <hinanshis4@gmail.com>
2022-02-09 14:01:33 +0530

    .gitignore

--- components/js/components.js
+++
@@ -1,6 +0,0 @@
-const alertCloseBox = document.querySelector("#alert-close");
-const alertCloseBtn = document.querySelector("#alert-close-btn");
-
-alertCloseBtn.addEventListener("click",()=>{
-    alertCloseBox.style.display = "none";
-})
