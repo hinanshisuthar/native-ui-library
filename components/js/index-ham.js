c2fa4107bff7ff923b3714729adee00343c2fc6d
Hinanshi Suthar <hinanshis4@gmail.com>
2022-02-21 02:27:03 +0530

    fix: removed comments, alignment

--- components/js/index-ham.js
+++ components/js/index-ham.js
@@ -1,13 +1,18 @@
 const openMenu = document.querySelector(".menu")
 const hamIcon = document.querySelector("#index-ham")
+const homeMenuLinks = document.querySelectorAll(".home-menu-links");
 
 hamIcon.addEventListener("click", menu);
 
-    function menu() {
-        // console.log("here");
-        if(openMenu.style.display === "block") {
-            openMenu.style.display = "none";
-        } else {
+function menu() {
+    if (openMenu.style.display === "block") {
+        openMenu.style.display = "none";
+    } else {
         openMenu.style.display = "block";
-        }
     }
+}
+
+homeMenuLinks.forEach("click", () => {
+    openMenu.style.display = "none";
+});
+
