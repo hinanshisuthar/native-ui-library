35470b948e910eba150398bed6c15b000e1a8417
Hinanshi Suthar <hinanshis4@gmail.com>
2022-10-10 18:08:29 +0530

    [Fix]: remove console.logs, dynamic rating

--- components/js/toast.js
+++ components/js/toast.js
@@ -3,12 +3,10 @@ const dismissToast = document.querySelector("#cancel-toast");
 const closeToast = document.querySelector("#demo-toast");
 
 openToast.addEventListener("click", () => {
-    console.log("here");
     closeToast.style.display = "flex";
 });
 
 dismissToast.addEventListener("click", () => {
-    console.log("here2");
     closeToast.style.display = "none";
 });
 
